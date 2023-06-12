1f09fdb2eaba5c44657f3cb1e3c2bd14bd92ee95
Josoa Tahina <jt.raobelina@gmail.com>
2023-06-12 18:31:35 +0300

    Add function login in UserController

--- controller/UserController.php
+++ controller/UserController.php
@@ -7,4 +7,16 @@ class UserController extends Users
         $data['is_admin'] = false;
         return parent::addUser($data);
     }
+
+    public function login($data)
+    {
+        $user = $this->getUserByUsername($data['username']);
+        if($user->num_rows > 0) {
+            $user = $user->fetch_assoc();
+            if($user['psswd'] == $data['psswd']) {
+                $_SESSION['user'] = $data['username'];
+                return 1;
+            }
+        }
+    }
 }
