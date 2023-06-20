e47eff99c3ebe4cda87bca1dc223575b90d1667b
Josoa Tahina <jt.raobelina@gmail.com>
2023-06-20 19:48:20 +0300

    Fix navbar responsive in front

--- views/partial/header.php
+++ views/partial/header.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>QCM</title>
         <link href="assets/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
         <link href="assets/css/style.css" type="text/css" rel="stylesheet" />
--- views/partial/nav.php
+++ views/partial/nav.php
@@ -1,15 +1,15 @@
-<nav class="navbar navbar-expand navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item <?php if(!isset($_REQUEST['c'])) { echo 'active'; } ?>">
                 <a class="nav-link" href="/qcm/">Accueil</a>
             </li>
-        <li class="nav-item <?php if(isset($_REQUEST['c']) && $_REQUEST['c'] == 'Questionnaire') { echo 'active'; } ?>">
-            <a class="nav-link" href="?c=Questionnaire">Questionnaire</a>
-        </li>
-        <li class="nav-item <?php if(isset($_REQUEST['c']) && $_REQUEST['c'] == 'Data') { echo 'active'; } ?>">
-            <a class="nav-link" href="?c=Data">Mes réponses</a>
-        </li>
+            <li class="nav-item <?php if(isset($_REQUEST['c']) && $_REQUEST['c'] == 'Questionnaire') { echo 'active'; } ?>">
+                <a class="nav-link" href="?c=Questionnaire">Questionnaire</a>
+            </li>
+            <li class="nav-item <?php if(isset($_REQUEST['c']) && $_REQUEST['c'] == 'Data') { echo 'active'; } ?>">
+                <a class="nav-link" href="?c=Data">Mes réponses</a>
+            </li>
         </ul>
         <ul class="navbar-nav my-2 my-lg-0">
             <li class="nav-item dropdown">
@@ -21,3 +21,29 @@
         </ul>
     </div>
 </nav>
+<div class="pos-f-t nav-mobile">
+    <nav class="navbar navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMenu" aria-controls="navMenu" aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <span class="text-white"><?php echo $_SESSION['user']; ?></span>
+    </nav>
+    <div class="collapse navbar-dark" id="navMenu">
+        <div class="bg-dark p-2">
+            <ul class="navbar-nav">
+                <li class="nav-item <?php if(!isset($_REQUEST['c'])) { echo 'active'; } ?>">
+                    <a class="nav-link" href="/qcm/">Accueil</a>
+                </li>
+                <li class="nav-item <?php if(isset($_REQUEST['c']) && $_REQUEST['c'] == 'Questionnaire') { echo 'active'; } ?>">
+                    <a class="nav-link" href="?c=Questionnaire">Questionnaire</a>
+                </li>
+                <li class="nav-item <?php if(isset($_REQUEST['c']) && $_REQUEST['c'] == 'Data') { echo 'active'; } ?>">
+                    <a class="nav-link" href="?c=Data">Mes réponses</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?c=Index&m=logout">Déconnexion</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
